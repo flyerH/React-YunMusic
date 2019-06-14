e1ec976f0004acebc34ab0efa8f5087f06a0daf3
flyerH <hzw758@qq.com>
2019-06-14 10:43:04 +0800

    升级依赖&build

---
+++ build/precache-manifest.7f5a1b7b3fa81519644b49068d7296a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e2b9a11953fcfb281ac2f281bdd332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6812f704147527b381b9",
+    "url": "/static/css/main.cf5a7fb7.chunk.css"
+  },
+  {
+    "revision": "0ec1ecc6ecedc7abe14b",
+    "url": "/static/js/2.7cab7dc2.chunk.js"
+  },
+  {
+    "revision": "6812f704147527b381b9",
+    "url": "/static/js/main.3b0bc70d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1233a1c24c86357a22b1e50a2dc272c5",
+    "url": "/static/media/LOVE_static.1233a1c2.jpg"
+  },
+  {
+    "revision": "cc296c7e1a479524ff75b6f7266509fc",
+    "url": "/static/media/hotsong.cc296c7e.png"
+  },
+  {
+    "revision": "dc801d8516416a06a32e6a4cf87eeb6a",
+    "url": "/static/media/newsong.dc801d85.png"
+  },
+  {
+    "revision": "507862703b08494c5223aa3878de62ee",
+    "url": "/static/media/origin.50786270.png"
+  },
+  {
+    "revision": "2eaf2cdc725e29b5570800a0b03e862f",
+    "url": "/static/media/singer.2eaf2cdc.png"
+  },
+  {
+    "revision": "607d558c0c77d33b98b100f5e16d715f",
+    "url": "/static/media/softName.607d558c.png"
+  }
+]);
